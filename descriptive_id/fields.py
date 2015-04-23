4f6a531b262ea1bce3c887f13e53551de7098292
Bentley Cook <bentley.s.cook@gmail.com>
2015-04-23 09:15:06 -0400

    initial fieldwork

---
+++ descriptive_id/fields.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import string
+import random
+
+from django import forms
+from django.db.models import CharField
+
+
+class DescriptiveIDField(CharField):
+
+    def __init__(self, prepend=None, auto=True, *args, **kwargs):
+        if not prepend:
+            raise Exception
+        else:
+            self.prepend = prepend
+        self.auto = auto
+
+        kwargs['max_length'] = len(self.prepend) + 15
+
+        if auto:
+            # Do not let the user edit UUIDs if they are auto-assigned.
+            kwargs['editable'] = False
+            kwargs['blank'] = True
+            kwargs['unique'] = True
+        super(DescriptiveIDField, self).__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(DescriptiveIDField, self).deconstruct()
+        if self.auto:
+            kwargs.pop('editable')
+            kwargs.pop('blank')
+            kwargs.pop('unique')
+            kwargs['auto'] = True
+        return name, path, args, kwargs
+
+    def _generate_descriptive_id(self):
+        return '{prepend}_'.format(prepend=self.prepend) + ''.join(random.choice(string.digits + string.ascii_letters) for _ in range(14))
+
+    def pre_save(self, model_instance, add):
+        """
+        This is used to ensure that we auto-set values if required.
+        See CharField.pre_save
+        """
+        value = getattr(model_instance, self.attname, None)
+        if self.auto and add and not value:
+            # Assign a new value for this attribute if required.
+            descriptive_id = self._generate_descriptive_id()
+            setattr(model_instance, self.attname, descriptive_id)
+        return descriptive_id
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': forms.CharField,
+            'max_length': self.max_length,
+        }
+        defaults.update(kwargs)
+        return super(DescriptiveIDField, self).formfield(**defaults)
