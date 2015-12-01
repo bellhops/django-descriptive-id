18920ffb2d5e98ecd3d606f7b69490b28faf46fb
Mrhwick <matt.hardwick@getbellhops.com>
2015-12-01 14:31:54 -0500

    Make setup publish a thing

--- setup.py
+++ setup.py
@@ -1,16 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import re
+import os
+import sys
+from setuptools import setup
+
+name = 'descriptive_id'
+package = 'descriptive_id'
+description = 'django-descriptive-id is a field that you can use on your models to give them a human-readable, unique identifier.'
+url = 'https://github.com/bellhops/django-descriptive-id'
+author = 'Bellhops'
+author_email = 'tech@getbellhops.com'
+license = 'MIT'
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]",
+                     init_py, re.MULTILINE).group(1)
+
+version = get_version(package)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(version))
+    print("  git push --tags")
+    sys.exit()
 
 setup(
-    name='descriptive_id',
-    packages=['descriptive_id'],
-    version='0.1.2',
-    description='django-descriptive-id is an abstract model that you can use as a mixin to give your models a human-readable, unique identifier.',
-    author='Bellhops',
-    author_email='tech@getbellhops.com',
-    url='https://github.com/bellhops/django-descriptive-id',
+    name=name,
+    version=version,
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=[],
     download_url='https://github.com/bellhops/django-descriptive-id/tarball/0.1',
     keywords=['django', 'modelfield', 'uuid'],
     classifiers=[],
